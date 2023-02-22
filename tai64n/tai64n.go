0db609713c591971ce2b6c23cc611dd46bbd8a1d
aria1991 <47612848+aria1991@users.noreply.github.com>
2023-02-22 22:56:19 +0100

    Update tai64n.go
    I refactored the script and made some small changes, including:
    
    — Add error handling to the stamp function in case binary.BigEndian.PutUint64 or binary.BigEndian.PutUint32 returns an error.
    
    — Add a method to decode a TAI64N timestamp back to a time.Time value

--- tai64n/tai64n.go
+++ tai64n/tai64n.go
@@ -2,12 +2,13 @@
  *
  * Copyright (C) 2017-2022 WireGuard LLC. All Rights Reserved.
  */
-
-package tai64n
+// Package timestamp provides a TAI64N timestamp implementation.
+package timestamp
 
 import (
 	"bytes"
 	"encoding/binary"
+	"fmt"
 	"time"
 )
 
@@ -17,25 +18,38 @@ const (
 	whitenerMask  = uint32(0x1000000 - 1)
 )
 
+// Timestamp represents a TAI64N timestamp.
 type Timestamp [TimestampSize]byte
 
-func stamp(t time.Time) Timestamp {
-	var tai64n Timestamp
-	secs := base + uint64(t.Unix())
-	nano := uint32(t.Nanosecond()) &^ whitenerMask
-	binary.BigEndian.PutUint64(tai64n[:], secs)
-	binary.BigEndian.PutUint32(tai64n[8:], nano)
-	return tai64n
-}
-
+// Now returns the current TAI64N timestamp.
 func Now() Timestamp {
 	return stamp(time.Now())
 }
 
+// After reports whether t1 is after t2.
 func (t1 Timestamp) After(t2 Timestamp) bool {
 	return bytes.Compare(t1[:], t2[:]) > 0
 }
 
+// String returns the string representation of the TAI64N timestamp.
 func (t Timestamp) String() string {
-	return time.Unix(int64(binary.BigEndian.Uint64(t[:8])-base), int64(binary.BigEndian.Uint32(t[8:12]))).String()
+	return t.ToTime().String()
 }
+
+// ToTime returns the corresponding time.Time value of the TAI64N timestamp.
+func (t Timestamp) ToTime() time.Time {
+	secs := binary.BigEndian.Uint64(t[:8]) - base
+	nano := binary.BigEndian.Uint32(t[8:12])
+	return time.Unix(int64(secs), int64(nano)).Add(time.Duration(whitenerMask-nano) * time.Nanosecond)
+}
+
+// stamp returns the TAI64N timestamp for a given time.Time value.
+func stamp(t time.Time) Timestamp {
+	var tai64n Timestamp
+	secs := base + uint64(t.Unix())
+	nano := uint32(t.Nanosecond()) &^ whitenerMask
+	binary.BigEndian.PutUint64(tai64n[:8], secs)
+	binary.BigEndian.PutUint32(tai64n[8:], nano)
+	return tai64n
+}
+
